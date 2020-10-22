8066421949fa998e2788e4591431a50f4f7ce7db
Itay <naveh.itay@gmail.com>
2020-10-22 21:31:51 +0100

    added saturation and brightness sliders

--- hueSlider/sketch.js
+++ hueSlider/sketch.js
@@ -1,15 +1,31 @@
-let slider;
-let val;
+let hueVal;
+let hueSlider;
+
+let satVal;
+let satSlider;
+
+let briVal;
+let briSlider;
 
 function setup() {
   createCanvas(400, 400);
-  slider = createSlider(0, 255, 0, 1);
-  val = createP("Hue Value");
+  hueVal = createP("Hue Value");
+  hueSlider = createSlider(0, 255, 0, 1);
+
+  satVal = createP("Saturation Value");
+  satSlider = createSlider(0, 255, 255, 1);
+
+  briVal = createP("Brightness/Value Value");
+  briSlider = createSlider(0, 255, 255, 1);
+
   colorMode(HSB, 255);
 }
 
 function draw() {
-  // console.log(slider.value());
-  val.elt.innerHTML = slider.value();
-  background(slider.value(), 255, 255);
+  hueVal.elt.innerHTML = hueSlider.value();
+  satVal.elt.innerHTML = satSlider.value();
+  briVal.elt.innerHTML = briSlider.value();
+
+
+  background(hueSlider.value(), satSlider.value(), briSlider.value());
 }
